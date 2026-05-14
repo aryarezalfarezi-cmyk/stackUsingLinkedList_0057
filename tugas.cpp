233c5fc151aad4bbc0f652daabcf35ac018db843
aryarezalfarezi-cmyk <aryarezalfarezi@gmail.com>
2026-05-14 15:09:24 +0700

    isian dua dan akhir if else

--- tugas.cpp
+++ tugas.cpp
@@ -54,3 +54,10 @@ public:
         {
             Node *current = top;
             while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
