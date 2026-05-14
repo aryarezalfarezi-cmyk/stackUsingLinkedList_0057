b8123fc4e202b8786c43bdcd651db0deb3da6ea5
aryarezalfarezi-cmyk <aryarezalfarezi@gmail.com>
2026-05-14 15:07:33 +0700

    akhrir code empty

--- tugas.cpp
+++ tugas.cpp
@@ -40,3 +40,7 @@ public:
         {
             cout << "Stack is empty." << endl;
         }
+        Node *temp = top;
+        top = top->next;
+        cout << "Popped value: " << temp->data << endl;
+    }
